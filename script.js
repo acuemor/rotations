e3b4b9438febff29b7659650b5f2d1165d4d0d12
Abel Cuevas <abel.cuevas.contractor@bbva.com>
2025-06-30 09:32:39 +0200

    change start date

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function calculateRotations(startDate, today, teamMembers) {
 
 function rotateResponsibilities() {
     const today = new Date();
-    const startDate = new Date("2024-09-16"); // Fecha de inicio de la primera rotación
+    const startDate = new Date("2025-06-30"); // Fecha de inicio de la primera rotación
     const rotations = calculateRotations(startDate, today, teamMembers);
 
     const currentResponsible = rotations[0];
